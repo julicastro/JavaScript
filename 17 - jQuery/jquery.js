baa44be3967814702e2ec6700ad07544088c6c0a
julicastro <castroejulian@gmail.com>
2021-11-05 04:00:54 -0300

    .

---
+++ 17 - jQuery/jquery.js
@@ -0,0 +1,49 @@
+/*
+
+<script src="jquery-3.6.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+SYNTAX:
+
+$(selector).action()
+
+$(this).hide()
+$("p").hide()
+$(".test").hide()
+class="test".
+$("#test").hide()
+id="test".
+
+METHODS: 
+
+$(document).ready(function(){
+ // jQuery methods go here…
+});
+
+$(function(){
+  // jQuery methods go here...
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
